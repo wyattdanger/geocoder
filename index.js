2cfc97ebb806236214be29321899051d8b7cda55
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-07-15 15:07:37 -0400

    0.0.3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var http = require( 'http' );
  * Version
  */
 
-var version = '0.0.2';
+var version = '0.0.3';
 
 /**
  * Formats a given `loc` to submit to Google
@@ -108,7 +108,7 @@ Geocoder.prototype = {
    * @api public
    */
 
-  version: (function () { return version; })()
+  version: version
 
 };
 
