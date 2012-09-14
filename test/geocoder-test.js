8fd00d674f183091db9c12c626f45696e5893ffc
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-09-14 10:46:25 +0200

    Add reverse geocoding test

--- test/geocoder-test.js
+++ test/geocoder-test.js
@@ -16,6 +16,16 @@ module.exports = {
     });
   },
 
+  testReverseGeocode: function(test){
+    test.expect(3);
+    geocoder.reverseGeocode(49.101,6.1442, function(err, result){
+      test.ok(!err);
+      test.equals('OK', result.status);
+      test.ok(result.results[0].formatted_address.match(/Montigny-lès-Metz/));
+      test.done();
+    });
+  },
+
   testLanguage: function(test){
     test.expect(3);
     geocoder.geocode("Plattlinger Str. 10, 81479 München, Deutschland", function(err, result){
