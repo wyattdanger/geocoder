b48d7d86e044b56f06480304ad122092b1994bf6
nisanthsojan <nisanthsojan@yahoo.com>
2014-09-26 10:54:28 +0530

    added close bracket
    added close bracket

--- providers/google.js
+++ providers/google.js
@@ -1,39 +1,39 @@
 var request = require("request");
 var _ = require("underscore");
 
-exports.geocode = function ( providerOpts, loc, cbk, opts ) {
+exports.geocode = function (providerOpts, loc, cbk, opts) {
 
-  var proto = "http:",
-      options = _.extend({sensor: false, address: loc}, opts || {});
-      
-  if(_.has(options,"key"){
-    proto = "https:";
-  }
+    var proto = "http:",
+        options = _.extend({sensor: false, address: loc}, opts || {});
 
-  request({
-    uri:proto+"//maps.googleapis.com/maps/api/geocode/json",
-    qs:options
-  }, function(err,resp,body) {
-    if (err) return cbk(err);
-    cbk(null,JSON.parse(body));
-  });
+    if (_.has(options, "key")) {
+        proto = "https:";
+    }
+
+    request({
+        uri: proto + "//maps.googleapis.com/maps/api/geocode/json",
+        qs: options
+    }, function (err, resp, body) {
+        if (err) return cbk(err);
+        cbk(null, JSON.parse(body));
+    });
 };
 
-exports.reverseGeocode = function ( providerOpts, lat, lng, cbk, opts ) {
+exports.reverseGeocode = function (providerOpts, lat, lng, cbk, opts) {
 
-  var proto = "http:",
-      options = _.extend({sensor: false, latlng: lat + ',' + lng}, opts || {});
-      
-  if(_.has(options,"key"){
-    proto = "https:";
-  }
+    var proto = "http:",
+        options = _.extend({sensor: false, latlng: lat + ',' + lng}, opts || {});
 
-  request({
-    uri:proto+"//maps.googleapis.com/maps/api/geocode/json",
-    qs:options
-  }, function(err,resp,body) {
-    if (err) return cbk(err);
-    cbk(null,JSON.parse(body));
-  });
+    if (_.has(options, "key")) {
+        proto = "https:";
+    }
+
+    request({
+        uri: proto + "//maps.googleapis.com/maps/api/geocode/json",
+        qs: options
+    }, function (err, resp, body) {
+        if (err) return cbk(err);
+        cbk(null, JSON.parse(body));
+    });
 
 };
