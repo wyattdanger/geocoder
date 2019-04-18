0fc4e5b82dac317b1a6acddd8b6f8be57c6bfe16
Secrets Of Sumé <secretsofsume@gmail.com>
2019-04-18 00:02:20 -0300

    Add Api Key in Google Provider

--- providers/google.js
+++ providers/google.js
@@ -1,44 +1,50 @@
 var request = require("request");
-var _ = require('underscore');
+var _ = require("underscore");
 
-exports.geocode = function ( providerOpts, loc, cbk, opts ) {
-
-  var options = _.extend({sensor: false, address: loc}, opts || {});
-  var uri = "http" + ( options.key ? "s" : "" ) + "://maps.googleapis.com/maps/api/geocode/json"
-  request({
-    uri: uri,
-    qs:options
-  }, function(err,resp,body) {
-    if (err) return cbk(err);
-    var result;
-    try {
-      result = JSON.parse(body);
-    } catch (err) {
-      cbk(err);
-      return;
+exports.geocode = function(providerOpts, loc, key, cbk, opts) {
+  var options = _.extend({ sensor: false, address: loc }, opts || {});
+  var uri = "https://maps.googleapis.com/maps/api/geocode/json?key=" + key;
+  request(
+    {
+      uri: uri,
+      qs: options
+    },
+    (err, resp, body) => {
+      if (err) return cbk(err);
+      var result;
+      try {
+        result = JSON.parse(body);
+      } catch (err) {
+        cbk(err);
+        return;
+      }
+      cbk(null, result);
     }
-    cbk(null,result);
-  });
+  );
 };
 
-exports.reverseGeocode = function ( providerOpts, lat, lng, cbk, opts ) {
+exports.reverseGeocode = function(providerOpts, lat, lng, key, cbk, opts) {
+  var options = _.extend(
+    { sensor: false, latlng: lat + "," + lng },
+    opts || {}
+  );
+  var uri = "https://maps.googleapis.com/maps/api/geocode/json?key=" + key;
 
-  var options = _.extend({sensor: false, latlng: lat + ',' + lng}, opts || {});
-  var uri = "http" + ( options.key ? "s" : "" ) + "://maps.googleapis.com/maps/api/geocode/json"
-
-  request({
-    uri:uri,
-    qs:options
-  }, function(err,resp,body) {
-    if (err) return cbk(err);
-    var result;
-    try {
-      result = JSON.parse(body);
-    } catch (err) {
-      cbk(err);
-      return;
+  request(
+    {
+      uri: uri,
+      qs: options
+    },
+    (err, resp, body) => {
+      if (err) return cbk(err);
+      var result;
+      try {
+        result = JSON.parse(body);
+      } catch (err) {
+        cbk(err);
+        return;
+      }
+      cbk(null, result);
     }
-    cbk(null,result);
-  });
-
+  );
 };
